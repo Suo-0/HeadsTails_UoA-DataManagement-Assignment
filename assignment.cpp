f55774332a2e77e61d6d4cf472898fa027420597
Suo-0 <131347382+Suo-0@users.noreply.github.com>
2024-07-04 23:47:11 +0900

    Final revision

--- assignment.cpp
+++ assignment.cpp
@@ -1,15 +1,26 @@
 #include<iostream>
+#include<cstdlib>
 using namespace std;
 
 int main(){
     int hCnt=0, tCnt=0;
+    string usr;
+
+    cout << "Who are you?" << endl << "> ";
+    cin >> usr;
+    cout << "Hello, " << usr << "!" << endl;
     cout << "Tossing a coin..." << endl;
+
     for(int i = 1; i <= 3; i++){
         int r = rand();
-        cout << "Round 1: " << (r%2 == 0 ? "Heads" : "Tails") << endl;
+        cout << "Round " << i << ": " << (r%2 == 0 ? "Heads" : "Tails") << endl;
         if(r%2) tCnt++;
         else hCnt++;
     }
+
     cout << "Heads: " << hCnt << ", Tails: " << tCnt << endl;
+    if(hCnt > tCnt){cout << usr << "won!" << endl;}
+    else{cout << usr << " lost!" << endl;}
+
     return 0;
 }
