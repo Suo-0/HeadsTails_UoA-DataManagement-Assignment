c62130557ede4cca10c2789ca88b8da3dfe7a47c
Suo-0 <131347382+Suo-0@users.noreply.github.com>
2024-07-05 01:50:44 +0900

    HoT: initial revision

---
+++ re_re_assignment.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    int hCnt = 0, tCnt = 0;
+
+    cout << "Tossing a coin..." << endl;
+
+    srand(time(0));
+    for (int i = 1; i <= 3; i++) {
+        int r = rand();
+        cout << "Round " << i << ": " << (r % 2 == 0 ? "Heads" : "Tails") << endl;
+        if (r % 2) tCnt++;
+        else hCnt++;
+    }
+
+    cout << "Heads: " << hCnt << ", Tails: " << tCnt << endl;
+
+    return 0;
+}
